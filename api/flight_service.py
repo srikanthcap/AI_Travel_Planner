f5a95b3359f65385232cd3f6ff268ae3a4bda872
srikanthcap <srikanthsrikanth200511@gmail.com>
2026-06-25 17:53:35 +0530

    Add project structure placeholders

---
+++ api/flight_service.py
@@ -0,0 +1,8 @@
+def get_flight_details(source, destination):
+
+    return {
+        "source": source,
+        "destination": destination,
+        "estimated_duration": "2 - 5 Hours",
+        "note": "Please check airline websites for real-time prices."
+    }
